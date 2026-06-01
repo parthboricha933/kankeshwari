e54a06f257acf7cc570f810bbfcb3dbb85fdbd8b
Z User <z@container>
2026-06-01 05:35:02 +0000

    f40721d1-d255-4fbc-bdcb-f4d017a15942

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function Home() {
+  return (
+    <iframe
+      src="/sea-cafe-diu.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        display: 'block',
+      }}
+      title="Sea Cafe & Food Destination – Diu"
+    />
+  )
+}
